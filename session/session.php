1dd1acb9c95d74e7ec4a0afa0795bd364b13292e
aymensellaouti <aymen.sellaouti@gmail.com>
2023-12-15 15:56:55 +0100

    session exemple

---
+++ session/session.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+   1 Si c'est la première visite => elle va afficher 'bienvenu chez nous' 
+   2 Si c'est la néme visite => elle va afficher Bonjour, merci pour votre fidélité, c'est votre {n}éme visite
+*/
+
+session_start();
+
+// Si ma variable nbVisite existe c'est que je suis déjà passé par la 
+if(isset($_SESSION['nbVisite'])) {
+    // J'incrémente mon nombre de visite de 1 puisque je fait une visite
+    $nb = $_SESSION['nbVisite'] + 1;
+    // j'affiche
+    echo "Bonjour, merci pour votre fidélité, c'est votre $nb éme visite";
+    // Je met à jour ma session
+    $_SESSION['nbVisite'] = $nb;
+// Le else c'est ma première visite
+} else {
+    $_SESSION['nbVisite'] = 1;  
+    echo 'Bienvenu chez nous'; 
+}
